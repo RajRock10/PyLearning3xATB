047db967595906c0fc660cd6438fa8dafdfc1151
RAJESHWAR <saharajeshwarlm2021@gmail.com>
2024-06-16 18:05:40 +0530

    task1a

---
+++ Ex_10062024/Task1a.py
@@ -0,0 +1,12 @@
+# Input number from the user
+num = float(input("Enter a number: "))
+
+# Calculate Square and Cube of the number
+square = num ** 2
+cube = num ** 3
+
+# Print the results
+print(f"Number: {num}")
+print(f"Square: {square}")
+print(f"Cube: {cube}")
+
