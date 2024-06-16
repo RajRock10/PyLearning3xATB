b47155c26b9050db348ca55dd485964efb71d268
RAJESHWAR <saharajeshwarlm2021@gmail.com>
2024-06-16 18:13:05 +0530

    Task1b.py

--- Ex_10062024/Task1b.py
+++ Ex_10062024/Task1b.py
@@ -1,3 +1,5 @@
+# Create a python program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+
 # Input numbers from the user
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
