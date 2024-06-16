732195de79a1d254ac062d967ea6ea243eaee243
RAJESHWAR <saharajeshwarlm2021@gmail.com>
2024-06-16 18:10:37 +0530

    task1a

--- Ex_10062024/Task1a.py
+++ Ex_10062024/Task1a.py
@@ -1,3 +1,4 @@
+# One Develop Python script that calculates the square and cube of a given number.
 # Input number from the user
 num = float(input("Enter a number: "))
 
@@ -10,3 +11,6 @@ print(f"Number: {num}")
 print(f"Square: {square}")
 print(f"Cube: {cube}")
 
+
+# One Develop a one line Python script that calculates the square and cube of a given number.
+print("Square:", (num := float(input("Enter a number: ")))**2, "Cube:", num**3)
